3b5246530a23d0843f1fb83d285f7f9c011c78f3
berkaydasgil <43037121+berkaydasgil@users.noreply.github.com>
2021-02-08 18:19:28 +0100

    Time: 32 ms (60.49%), Space: 14.2 MB (78.19%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.py
@@ -0,0 +1,10 @@
+# tekrar coz dp ile. catalan numberida hatirla.
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in range(1, n+1):
+            for j in range(i):
+                res[i] += res[j] * res[i-1-j]
+        return res[n]
