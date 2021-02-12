8421162960bc9158b124320b7f3e3c0b896d5605
berkaydasgil <43037121+berkaydasgil@users.noreply.github.com>
2021-02-12 20:10:41 +0100

    Time: 148 ms (34.42%), Space: 17 MB (8.70%) - LeetHub

---
+++ single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1,len(nums),2):
+            if nums[i] == nums[i-1]:
+                print(nums[i], nums[i-1])
+                continue
+            else:
+                return nums[i-1]
+        return nums[-1]
+            
+        
